21be6e8e22fe12c619e3fcbf65aa65be2d5e32d1
donghaoran <donghaoran@wondersgroup.com>
2022-09-02 21:28:09 +0800

    1.修复使用FastDFS上传头像失败提示文件名没有后缀问题 2.ReadMe文档错误修复

--- vctgo-modules/vctgo-file/src/main/java/com/vctgo/file/controller/service/FastDfsSysFileServiceImpl.java
+++ vctgo-modules/vctgo-file/src/main/java/com/vctgo/file/controller/service/FastDfsSysFileServiceImpl.java
@@ -1,12 +1,12 @@
 package com.vctgo.file.controller.service;
 
-import org.apache.commons.io.FilenameUtils;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import com.vctgo.common.core.utils.file.FileTypeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import com.github.tobato.fastdfs.domain.fdfs.StorePath;
-import com.github.tobato.fastdfs.service.FastFileStorageClient;
 
 /**
  * FastDFS 文件存储
@@ -36,7 +36,7 @@ public class FastDfsSysFileServiceImpl implements ISysFileService
     public String uploadFile(MultipartFile file) throws Exception
     {
         StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(),
-                FilenameUtils.getExtension(file.getOriginalFilename()), null);
+                FileTypeUtils.getExtension(file), null);
         return domain + "/" + storePath.getFullPath();
     }
 }
